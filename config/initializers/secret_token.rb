e09ea75b4cf5bb6651bce5b2d398fcb31041cdc0
Shane M. Pelletier <lotios611@gmail.com>
2013-10-16 20:49:19 -0300

    Initial commit, add all files from directory tree. I haven't set up a git repo for this project yet, so there's a lot of work that has been done. The main thing that's been added is a map, but there have probably been other things that have been added/changed.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DontFeedTheMeter::Application.config.secret_key_base = '5141e34ae6554e770b59b0a07d2396f71c0c1d2bf0a50a4daf66d797ca011b9b39a6bd78c8e148ecfe3af3be6b52a17631f163d4ca2a0b6340181a8ba19555b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DontFeedTheMeter::Application.config.session_store :cookie_store, key: '_DontFeedTheMeter_session'
