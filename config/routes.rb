673097022956130b842667e2dfa7e6dda71aace3
Shane M. Pelletier <lotios611@gmail.com>
2013-10-28 20:40:47 -0300

    Add welcome screen to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 DontFeedTheMeter::Application.routes.draw do
-  get "map/index"
+  root "welcome#index"
+  get "map", to: "map#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'map#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
