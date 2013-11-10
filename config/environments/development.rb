92f87173fe590723d1292126f2817731e604fb77
Shane M. Pelletier <lotios611@gmail.com>
2013-11-09 21:39:54 -0400

    Add livereload for easier development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ DontFeedTheMeter::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.middleware.use Rack::LiveReload
 end
